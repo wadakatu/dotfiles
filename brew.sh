16c617a49b2c56f70fb57596edac7e7b7a435191
wadakatu <wadakatukoyo330@gmail.com>
2025-01-05 21:35:27 +0900

    wip

--- brew.sh
+++ brew.sh
@@ -27,4 +27,6 @@ echo "run brew autoremove ..."
 brew autoremove || { echo "brew autoremove failed" >&2; exit 1; }
 
 echo "run brew doctor ..."
+brew uses --installed openssl@1.1
+brew uses --installed ruby@3.0
 brew doctor || { echo "brew doctor failed" >&2; exit 1; }
